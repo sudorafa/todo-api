cc3fb76737a1f1d67a88abcfb8730f5e367f88a1
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2021-06-29 00:07:52 -0300

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
